876907039a7f91b50d9cdef5ee3ca27350498ae6
justSmK <28645140+justSmK@users.noreply.github.com>
2023-09-03 12:53:40 +0300

    Init project

---
+++ UIKitMarathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMarathon
+//
+//  Created by Sergei Semko on 9/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
